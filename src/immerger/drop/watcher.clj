9f5915b2b5085d554ca720f7662f5710c76ed56d
Tamas Oltyan <oltyant@gmail.com>
2016-06-20 17:22:53 +0200

    [++] Fix file watcher test

--- src/immerger/drop/watcher.clj
+++ src/immerger/drop/watcher.clj
@@ -1,30 +1,23 @@
 (ns immerger.drop.watcher
   (:require [hawk.core :as hawk]
             [immerger.web.state :as web]
-            ;;[immerger.log :as log]
-            ))
+            [clojure.java.io :as io]))
 
 (def observers (atom #{}))
 
 (defn subscribe [observer]
-  (swap! observers conj observer)
-  (println observer " is subscribed!"))
+  (swap! observers conj observer))
 
 (defn publish [file]
   (map #(apply % file) @observers)
-  (println "file: " file " is observed!"))
+  (io/delete-file file))
 
-(def watcher (agent ""))
-
-(defn start-watch [] (send-off watcher
-                               #(hawk/watch! %)
-                               [{:paths [@web/drop-path]
-                                 :handler (fn [ctx e]
-                                            (when (:file e)
-                                              (println (str "Watcher poll: " @web/drop-path " found " (:file e) " with change reason: " (:kind e)))
-                                              ;;(log/debug (str "Watcher poll: " web/@drop-path " found " (:file e) " with change reason: " (:kind e)))
-                                              
-                                              (publish (:file e)))
-                                            )}]))
+(defn start-watch [] (future
+                       (hawk/watch! 
+                        [{:paths [@web/drop-path]
+                          :handler (fn [ctx e]
+                                     (when (and (:file e) (= :create (:kind e)))
+                                       (println (str "Watcher poll: " @web/drop-path " found " (:file e) " with change reason: " (:kind e)))
+                                       (publish (:file e))))}])))
 
 
--- test/immerger/drop/watcher_test.clj
+++ test/immerger/drop/watcher_test.clj
@@ -1,23 +1,22 @@
 (ns immerger.drop.watcher-test
   (:require [immerger.drop.watcher :as watcher]
             [immerger.web.state :as web]
-            [clojure.java.io :as io]))
-(use 'clojure.test)
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]))
 
 (def file-name "./test/test_file")
 
 (defn test-fixture [test]
         (test)
-        (io/delete-file file-name))
+        (when (.exists (io/file file-name)) (io/delete-file file-name)))
 
 (use-fixtures :once test-fixture)
 
 (deftest drop-a-file
-  (let [res (ref (java.io.File. "unwatched_file"))]
+  (testing "Simple file drop"
     (web/set-drop-path "./test")
-    (watcher/subscribe #(ref-set res %))
+    (watcher/subscribe #((is (= (slurp file-name) (slurp %)))))
     (watcher/start-watch)
+    (Thread/sleep 1000)
     (spit file-name file-name)
-    (loop [] (when (not  (.exists @res)) (do (java.lang.Thread/sleep 500) (recur))))
-    (shutdown-agents)
-    (is (= (slurp file-name) (slurp @res)))))
+    (Thread/sleep 1000)))
