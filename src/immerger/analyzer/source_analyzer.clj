3d0596a47a56eed49597d54b7583a22c78d65b0a
Tamas Oltyan <oltyant@gmail.com>
2016-10-02 02:20:48 +0200

    [++] Add analyzer functions and tests

--- src/immerger/analyzer/source_analyzer.clj
+++ src/immerger/analyzer/source_analyzer.clj
@@ -1,13 +1,20 @@
 (ns immerger.analyzer.source-analyzer
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn line-count [f]
-  (with-open [rdr (io/reader f)]
-    (count (line-seq rdr))))
+(defn get-lines [f]
+  (str/split (slurp f) #"\n"))
 
-(defn average-line-length [f]
-  (with-open [rdr (io/reader f)]
-    (let [input-lines (line-seq rdr)
-          line-count (count input-lines)
-          char-count (reduce #(+ %1 (count %2)) 0 input-lines)]
-      (float (/ char-count line-count)))))
+(defn count-lines [lines]
+  (count lines))
+
+(defn avg-line-length [lines]
+  (let [line-count (count lines)
+        char-count (reduce #(+ %1 (count (str/trim %2))) 0 lines)]
+    (float (/ char-count line-count))))
+
+(defn count-non-reserved-chars [reserves lines]
+  (let [clean-lines (map #(str/replace % #"\s" "") lines)
+        content (seq (apply str clean-lines))
+        non-reserves (filter #(not (contains? reserves %)) content)]
+    (frequencies non-reserves)))
--- test/immerger/analyzer/source_analyzer_test.clj
+++ test/immerger/analyzer/source_analyzer_test.clj
@@ -4,21 +4,40 @@
             [clojure.java.io :as io]))
 
 (def test-file-name "./test/src_file")
+(def test-file-content1 "1\n2\n3")
+(def test-file-content2 "1111\n111\n11")
+(def test-file-content3 "(aa (bb (ccc cc c))")
+
+(defn delete-files [fs]
+  (doseq [f fs]
+    (when (.exists (io/file f) (io/delete-file f)))))
 
 (defn test-fixture [test]
   (test)
-  (when (.exists (io/file)) (io/delete-file test-file-name)))
+  (delete-files [test-file-name]))
 
-(deftest line-count-test
+(deftest count-lines-test
   (testing "Vanilla line count test"
-    (let [content "1\n2\n3"
-          expected-count 3]
-      (spit test-file-name content)
-      (is (= (analyzer/line-count test-file-name) expected-count)))))
+    (let [expected-count 3]
+      (spit test-file-name test-file-content1)
+      (is (= (analyzer/count-lines (analyzer/get-lines test-file-name)) expected-count)))))
 
-(deftest average-line-length-test
-  (testing "Vanilla average line length"
-    (let [content "1111\n11\n111\n1"
-          expected-result 2.5]
-      (spit test-file-name content)
-      (is (= (analyzer/average-line-length test-file-name) expected-result)))))
+(deftest avg-line-length-test
+  (testing "Vanilla average line length test"
+    (let [expected-result 3.0]
+      (spit test-file-name test-file-content2)
+      (is (= (analyzer/avg-line-length (analyzer/get-lines test-file-name)) expected-result)))))
+
+(deftest count-non-reserved-chars-test
+  (testing "Vanilla count-non-reserved-chars count test"
+    (let [reserves #{\( \)}
+          expected-result {\a 2 \b 2 \c 6}]
+      (spit test-file-name test-file-content3)
+      (is (= (analyzer/count-non-reserved-chars reserves (analyzer/get-lines test-file-name)) expected-result)))))
+
+(deftest count-non-reserved-keywords-test
+  (testing "Vanilla count-non-reserved-keywords"
+    (let [reserved-words #{"require" "defn" "def" "let" "recur" "loop"}
+          expected-result 5]
+      (spit test-file-name test-file-content4)
+      (is (= (analyzer/count-non-reserved-keywords reserved-words (analyzer/get-lines test-file-name)) expected-result)))))
