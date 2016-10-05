8f860e98221253ea1381d4126fcf7896219ef4af
Tamas Oltyan <oltyant@gmail.com>
2016-10-05 09:51:58 +0200

    [!!] Simplify test script call owing to Travis CI issue (time script not supported)

--- test/immerger/executor/runner_test.clj
+++ test/immerger/executor/runner_test.clj
@@ -4,9 +4,9 @@
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(def test-python-cmd ["time" "python" "-c" "'print [(x,y) for x in xrange(100) for y in xrange(100) if x != y]'"])
+(def test-python-cmd ["ls" "-larth"])
 
 (deftest command-line-runner-test
   (testing "Vanilla command line runner test"
     (let [result (exec/execute-command test-python-cmd)]
-      (is (> (count (:err result)) 0)))))
+      (is (> (count (:out result)) 0)))))
